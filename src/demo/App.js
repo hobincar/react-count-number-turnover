250663dbbd5d4d80b92fb51668500b4f32cfd5b9
Hobin Ryu <hobincar@kaist.ac.kr>
2018-02-24 14:49:30 +0900

    Make 'springConfig' as a prop of TurnOverNumber Component & Change values(stiffness, damping) of it

--- src/demo/App.js
+++ src/demo/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { TurnOverSequence } from '../lib';
 
 const App = () => (
-  <TurnOverSequence>
-    123,123,123,123,123,123
+  <TurnOverSequence style={{
+    fontSize: '60px',
+    fontWeight: 300,
+    color: '#1f4d6a',
+  }}>
+    123,123,123
   </TurnOverSequence>
 );
 
--- src/lib/components/TurnOverNumber.js
+++ src/lib/components/TurnOverNumber.js
@@ -2,7 +2,7 @@ import React  from 'react';
 import PropTypes from 'prop-types';
 import TextLoop from './TextLoop';
 
-const TurnOverNumber = ({ children, start, speed, onEnd, style }) => {
+const TurnOverNumber = ({ children, start, speed, springConfig, onEnd, style }) => {
   if (start === children) {
     return (
       <TextLoop
@@ -45,7 +45,7 @@ const TurnOverNumber = ({ children, start, speed, onEnd, style }) => {
   return (
     <TextLoop
       speed={speed}
-      springConfig={{ stiffness: 180, damping: 8 }}
+      springConfig={springConfig}
       nCall={start > end ? 2 : end - start + 1}
       onEnd={onEnd}
       style={style}
@@ -65,6 +65,10 @@ TurnOverNumber.propTypes = {
     PropTypes.number.isRequired,
   ]),
   speed: PropTypes.number.isRequired,
+  springConfig: PropTypes.shape({
+    stiffness: PropTypes.number.isRequired,
+    damping: PropTypes.number.isRequired
+  }).isRequired,
   onEnd: PropTypes.func,
   style: PropTypes.object,
 };
@@ -72,6 +76,10 @@ TurnOverNumber.propTypes = {
 TurnOverNumber.defaultProps = {
   start: 0,
   speed: 100,
+  springConfig: {
+    stiffness: 100,
+    damping: 10,
+  },
 };
 
 export default TurnOverNumber;
